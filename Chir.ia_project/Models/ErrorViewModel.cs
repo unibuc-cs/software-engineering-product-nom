cdd94fbc171f996e5fa6439355104000202aae72
Fabi <fabianflorete@yahoo.com>
2024-11-22 12:51:36 +0200

    Remove project, start again

--- Chir.ia_project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Chir.ia_project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
